9ab10d2e637fa1eed82a8f27d10e4be35901257b
Anuar Harb <anuarharb@hotmail.com>
2018-06-07 16:33:31 -0500

    backtips

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
 (function(){
-  'use strict'
+  'use strict';
 
   // Init DOM variables
-  const awesome = document.getElementById('awesome')
-  const load = document.getElementById('load')
+  const awesome = document.getElementById('awesome');
+  const load = document.getElementById('load');
   const loader = document.getElementsByClassName('loader')[0];
 
   // Intit load button listener
-  document.getElementById('load').addEventListener("click", function() {
+  load.addEventListener("click", () => {
     loadProgress(0);
   });
 
   // Transition
   function loadFunction(nextWidth) {
     let transitionTime = loadSpeed();
-    console.log('loadSpeed', transitionTime + 's');
-    loader.style.transitionDuration = transitionTime + 's';
-    loader.style.width = nextWidth + '%';
+    // console.log('loadSpeed', `${transitionTime}s`);
+    loader.style.transitionDuration = `${transitionTime}s`;
+    loader.style.width = `${nextWidth}%`;
 
-    setTimeout(function(){
+    setTimeout(() => {
       loadProgress(nextWidth);
-    }, transitionTime*1000);
+    }, transitionTime * 1000);
   }
 
   // Random transition time for animation
   function loadSpeed() {
-    let transitionTime = Math.ceil(Math.random()*8);
+    let transitionTime = Math.ceil(Math.random() * 8);
     return transitionTime;
   }
 
@@ -36,8 +36,9 @@
       load.innerHTML = "yey!";
       console.log('finish');
     }else {
-      let nextWidth = stepWidth + Math.round(Math.random()*(100 - stepWidth));
-      console.log('Progress',stepWidth + '->' + nextWidth);
+      let randomWith = Math.round(Math.random() * (100 - stepWidth))
+      let nextWidth = stepWidth + randomWith;
+      console.log('Progress', stepWidth + '->' + nextWidth);
       loadFunction(nextWidth);
     }
   }
